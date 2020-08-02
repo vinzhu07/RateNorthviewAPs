eb6a1d726ab24ef0472e1f2cb5d637549d8ce9c1
Vincent <vinzhu07@gmail.com>
2020-08-02 17:20:42 -0400

    db

--- functions.php
+++ functions.php
@@ -70,7 +70,7 @@ if ($database == null) {
 
 // connect to database
 //$db = mysqli_connect('us-cdbr-east-05.cleardb.net', 'bd6a0fdfffe95b', 'c59a7820');
-/*
+
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
@@ -79,10 +79,10 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $db = new mysqli($server, $username, $password, $db);
-*/
 
+/*
 //debug_to_console($db);
-$db = mysqli_connect($host, $username, "", "comment-reply-system");
+$db = mysqli_connect($host, $username, "", "comment-reply-system");*/
 $class_name = "";
 $class = "";
 
